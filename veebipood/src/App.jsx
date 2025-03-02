eadb10aa2640e712ceca4b187e12422f10c7c391
Raiko-Rajalaane <raiko.rajalaane@gmail.com>
2025-03-02 12:51:16 +0200

    uus rakendus

---
+++ veebipood/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+import { Link, Route, Routes } from "react-router-dom";
+import Avaleht from './pages/Avaleht';
+import Ostukorv from './pages/Ostukorv';
+import Seaded from './pages/Seaded';
+import Esindused from './pages/Esindused';
+import LisaToode from './pages/LisaToode';
+import Menu from './components/Menu';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <Menu />
+
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+
+        <Routes>
+          <Route path='/' element={ <Avaleht /> } />
+          <Route path='/ostukorv' element={ <Ostukorv /> } />
+          <Route path='/seaded' element={ <Seaded /> } />
+          <Route path='/lisa-toode' element={ <LisaToode /> } />
+          <Route path='/esindused' element={ <Esindused /> } />
+        </Routes>
+        
+    </>
+  )
+}
+
+export default App
---
+++ veebipood/src/components/Menu.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom"
+
+function Menu() {
+  return (
+    <div>
+   <Link to="/">
+    <img className="pilt" src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Nobe%2C_GIMS_2019%2C_Le_Grand-Saconnex_%28GIMS0061%29.jpg" alt="" />
+  </Link>
+
+  {/* <span className="tekst">Osta meie tooteid</span> */}
+
+  <Link to="/ostukorv">
+    <button>Ostukorvi</button>
+  </Link>
+
+  
+  <Link to="/seaded">
+    <button>Seaded</button>
+  </Link>
+
+  <Link to="/lisa-toode">
+    <button>Lisa toode</button>
+  </Link>
+
+  <Link to="/esindused">
+    <button>Esindused</button>
+  </Link>
+  </div>
+  )
+}
+
+export default Menu
