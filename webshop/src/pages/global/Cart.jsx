30001f3400a9e1c35ac6c1965350a23243d91f1a
Raiko-Rajalaane <raiko.rajalaane@gmail.com>
2025-04-09 02:14:33 +0300

    ostukorv localstorage-isse

--- webshop/src/pages/global/Cart.jsx
+++ webshop/src/pages/global/Cart.jsx
@@ -1,54 +1,66 @@
-import { useState } from "react"
-import cartFromFile from "../../data/cart.json"
-import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 function Cart() {
-  const [cart, setCart] = useState(cartFromFile);
+  const [cart, setCart] = useState(() => {
+    return JSON.parse(localStorage.getItem('cart')) || [];
+  });
   const { t } = useTranslation();
 
   const remove = (index) => {
-    cartFromFile.splice(index, 1);
-    setCart(cartFromFile.slice());
-  }
+    const updatedCart = cart.slice();
+    updatedCart.splice(index, 1);
+    setCart(updatedCart);
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+  };
 
   const clearCart = () => {
-    cartFromFile.length = 0;
-    setCart(cartFromFile.slice());
-  }
+    setCart([]); 
+    localStorage.removeItem('cart'); 
+  };
 
   const totalPrice = () => {
     let total = 0;
-    cart.forEach(product => total += product.price);
+    cart.forEach((product) => (total += product.price));
     return total;
-  }
+  };
 
   return (
     <div>
-      {cart.length === 0 && 
+      {cart.length === 0 && (
         <div>
-          {t("cart.empty")}<Link to='/' >{t("cart.browseLink")}</Link>
-        </div>
-      }
-
-      {cart.length > 0 &&
-        <>
-          <div>{t("cart.itemsCount")}{cart.length}</div>
-          <div>{t("cart.total")}{totalPrice()}</div>
-          <button onClick={clearCart}>{t("cart.clear")}</button>
-        </>  
-      }
-
-      {cart.map((product, index) =>
-        <div key={product.title}>
-          <img style={{"width": "100px"}} src={product.image} alt="" />  
-          <div>{product.title}</div>
-          <div>{product.price}</div>
-          <button onClick={() => remove(index)}>{t("cart.remove")}</button> <br /><br />
+          {t('cart.empty')}
+          <Link to="/">{t('cart.browseLink')}</Link>
         </div>
       )}
+
+      {cart.length > 0 && (
+        <>
+          <div>
+            {t('cart.itemsCount')}
+            {cart.length}
+          </div>
+          <div>
+            {t('cart.total')}
+            {totalPrice()}
+          </div>
+          <button onClick={clearCart}>{t('cart.clear')}</button>
+        </>
+      )}
+
+      {cart.map((product, index) => (
+        <div key={product.title}>
+          <img style={{ width: '100px' }} src={product.image} alt="" />
+          <div>{product.title}</div>
+          <div>{product.price}</div>
+          <button onClick={() => remove(index)}>{t('cart.remove')}</button>{' '}
+          <br />
+          <br />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
--- webshop/src/pages/global/HomePage.jsx
+++ webshop/src/pages/global/HomePage.jsx
@@ -1,84 +1,101 @@
-import { useState } from "react"
-import { toast } from "react-toastify";
-import productsFromFile from "../../data/products.json"
-import cartFromFile from "../../data/cart.json"
-import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import productsFromFile from '../../data/products.json';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 function HomePage() {
   const [products, setProducts] = useState(productsFromFile.slice());
   const { t } = useTranslation();
 
   const addToCart = (product) => {
-    cartFromFile.push(product);
-    toast.success(t("homepage.toastAdded"))
-  }
+    const oldCart = JSON.parse(localStorage.getItem('cart')) || [];
+    const updatedCart = oldCart.slice();
+    updatedCart.push(product);
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+    toast.success(t('homepage.toastAdded'));
+  };
 
   const sortAZ = () => {
     products.sort((a, b) => a.title.localeCompare(b.title));
     setProducts(products.slice());
-  }
+  };
 
   const sortZA = () => {
     products.sort((a, b) => b.title.localeCompare(a.title));
     setProducts(products.slice());
-  }
+  };
 
   const priceAsc = () => {
     products.sort((a, b) => a.price - b.price);
     setProducts(products.slice());
-  }
+  };
 
   const priceDesc = () => {
     products.sort((a, b) => b.price - a.price);
     setProducts(products.slice());
-  }
+  };
 
   const ratingBest = () => {
     products.sort((a, b) => b.rating.rate - a.rating.rate);
     setProducts(products.slice());
-  }
+  };
 
   const ratingWorst = () => {
     products.sort((a, b) => a.rating.rate - b.rating.rate);
     setProducts(products.slice());
-  }
+  };
 
   const filterCategory = (categoryName) => {
-    const filtered = productsFromFile.filter(product => product.category === categoryName);
+    const filtered = productsFromFile.filter(
+      (product) => product.category === categoryName
+    );
     setProducts(filtered);
-  }
+  };
 
   return (
     <div>
-      <button onClick={sortAZ}>{t("homepage.sortAZ")}</button>
-      <button onClick={sortZA}>{t("homepage.sortZA")}</button>
-      <button onClick={priceAsc}>{t("homepage.priceAsc")}</button>
-      <button onClick={priceDesc}>{t("homepage.priceDesc")}</button>
-      <button onClick={ratingBest}>{t("homepage.ratingBest")}</button>
-      <button onClick={ratingWorst}>{t("homepage.ratingWorst")}</button>
+      <button onClick={sortAZ}>{t('homepage.sortAZ')}</button>
+      <button onClick={sortZA}>{t('homepage.sortZA')}</button>
+      <button onClick={priceAsc}>{t('homepage.priceAsc')}</button>
+      <button onClick={priceDesc}>{t('homepage.priceDesc')}</button>
+      <button onClick={ratingBest}>{t('homepage.ratingBest')}</button>
+      <button onClick={ratingWorst}>{t('homepage.ratingWorst')}</button>
 
-      <br /><br />
-      <button onClick={() => filterCategory("men's clothing")}>{t("homepage.category.men")}</button>
-      <button onClick={() => filterCategory("women's clothing")}>{t("homepage.category.women")}</button>
-      <button onClick={() => filterCategory("jewelery")}>{t("homepage.category.jewelery")}</button>
-      <button onClick={() => filterCategory("electronics")}>{t("homepage.category.electronics")}</button>
+      <br />
+      <br />
+      <button onClick={() => filterCategory("men's clothing")}>
+        {t('homepage.category.men')}
+      </button>
+      <button onClick={() => filterCategory("women's clothing")}>
+        {t('homepage.category.women')}
+      </button>
+      <button onClick={() => filterCategory('jewelery')}>
+        {t('homepage.category.jewelery')}
+      </button>
+      <button onClick={() => filterCategory('electronics')}>
+        {t('homepage.category.electronics')}
+      </button>
 
-      <br /><br />
+      <br />
+      <br />
 
-      {products.map((product, index) =>
+      {products.map((product, index) => (
         <div key={product.id}>
-          <img style={{width: "100px"}} src={product.image} alt="" />
+          <img style={{ width: '100px' }} src={product.image} alt="" />
           <div>{product.title}</div>
           <div>{product.price}</div>
-          <Link to={"/product/" + index}><button>{t("homepage.viewProduct")}</button></Link>
-          <button onClick={() => addToCart(product)}>{t("homepage.addToCart")}</button>
+          <Link to={'/product/' + index}>
+            <button>{t('homepage.viewProduct')}</button>
+          </Link>
+          <button onClick={() => addToCart(product)}>
+            {t('homepage.addToCart')}
+          </button>
           <br /> <br />
         </div>
-      )}
+      ))}
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
